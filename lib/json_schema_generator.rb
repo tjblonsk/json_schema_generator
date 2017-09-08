8c13a142b555b4184b81eb14476241a6b2ead559
tblonski <tblonski@xogrp.com>
2017-09-08 17:58:11 -0400

    Init commit.

---
+++ lib/json_schema_generator.rb
@@ -0,0 +1,5 @@
+require "json_schema_generator/version"
+
+module JsonSchemaGenerator
+  # Your code goes here...
+end
---
+++ spec/json_schema_generator_spec.rb
@@ -0,0 +1,11 @@
+require "spec_helper"
+
+RSpec.describe JsonSchemaGenerator do
+  it "has a version number" do
+    expect(JsonSchemaGenerator::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
